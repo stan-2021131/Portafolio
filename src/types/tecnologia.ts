e610da6f4295cdb74e5145b827c19fafd2f6a9fe
stan-2021131 <sergiocoromac@gmail.com>
2026-06-01 00:31:54 -0600

    Interfaz para las tecologías

---
+++ src/types/tecnologia.ts
@@ -0,0 +1,21 @@
+type Category =
+  | "Backend"
+  | "Frontend"
+  | "Database"
+  | "Tools & Cloud"
+  | "Mobile";
+
+type Level = 1 | 2 | 3 | 4 | 5;
+
+interface Technology {
+  id: string;
+  name: string;
+  icon: string;
+  category: Category;
+  level: Level;
+  color: string;
+  description: string;
+}
+
+export type { Category, Level };
+export type { Technology };
